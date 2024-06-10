2029ebfa0689421b886455994617693d865f02fb
FelipeStillner <felipeeufranio@gmail.com>
2024-06-10 11:42:42 -0300

    .

--- a.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-section = pd.read_csv("section.csv")
-
-for index, row in section.iterrows():
-    date = str(row["date"]).split("/")
-    section.at[index, "date"] = date[2] + "-" + date[0] + "-" + date[1]
-
-section.to_csv("finalCsv/section.csv", index=False)
